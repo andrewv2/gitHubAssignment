12ee8cd5280181438fc91343252164b62ebf16cd
ANDREW VANVOORHEES <andrewvanvoorhees@Andrews-iMac.local>
2021-01-20 09:58:23 -0600

    initial commit

--- main.cpp
+++ main.cpp
@@ -3,3 +3,11 @@ Name: Andrew VanVoorhees
 Coding 02
 Purpose: "Hello World" cpp header
 **********************/
+
+#include "main.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
--- main.h
+++ main.h
@@ -3,3 +3,11 @@ Name: Andrew VanVoorhees
 Coding 02
 Purpose: "Hello World" .h header
 **********************/
+
+#ifndef HELLOWORLD_MAIN_H
+#define HELLOWORLD_MAIN_H
+
+#include <iostream>
+
+#endif //HELLOWORLD_MAIN_H
+
